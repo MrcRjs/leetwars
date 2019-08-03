6925dc91bbcbc336cb4fbba1e96ce89b0b8bac94
Marco A. Rojas <marco.rojas@alumno.buap.mx>
2019-08-03 00:06:10 -0400

    Narcissistic: readable test suites

--- narcissistic/Google_tests/narcissisticTest.cpp
+++ narcissistic/Google_tests/narcissisticTest.cpp
@@ -8,27 +8,27 @@
 
 Narcissistic narci;
 
-TEST(NarcissisticTestSuite, TRUENARCISSIST153){
+TEST(NarcissisticTestSuite, TrueNarci153){
 
     EXPECT_EQ(true,narci.run(153));
 }
 
-TEST(NarcissisticTestSuite, TRUENARCISSIST371){
+TEST(NarcissisticTestSuite, TrueNarci371){
 
     EXPECT_EQ(true,narci.run(371));
 }
 
-TEST(NarcissisticTestSuite, TRUENARCISSIST7){
+TEST(NarcissisticTestSuite, TrueNarci7){
 
     EXPECT_EQ(true,narci.run(7));
 }
 
-TEST(NarcissisticTestSuite, TRUENARCISSIST122){
+TEST(NarcissisticTestSuite, FalseNarci122){
 
     EXPECT_EQ(false,narci.run(122));
 }
 
-TEST(NarcissisticTestSuite, TRUENARCISSIST4887){
+TEST(NarcissisticTestSuite, FalseNarci4887){
 
     EXPECT_EQ(false,narci.run(4887));
 }
