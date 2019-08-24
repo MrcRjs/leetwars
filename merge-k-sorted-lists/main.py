dadc515f81e74c320dba8b010997851978ad263a
Marco A. Rojas <marco.rojas@alumno.buap.mx>
2019-08-24 01:09:18 -0400

    [WIP] Leetcode: Merge K Sorted Linked Lists v2

--- merge-k-sorted-lists/main.py
+++ merge-k-sorted-lists/main.py
@@ -2,52 +2,52 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+    def __str__(self):
+        return str(self.val)+ ' -> ' + str(self.next)
 
+
+
+def removeNones(listWithNones):
+    return list(filter(lambda x: x, listWithNones))
 
 class Solution:
     def mergeKLists(self, lists: [ListNode]):
         res = None
-        idx = 0
-        while res == None and len(lists) < idx:
-            res = lists[0]
-            idx += 1
-
-        for lN in lists[idx:]:
-            if lN:
-                res = self.mergeTwoLists(res, lN)
-        return res
-
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-
-        result = ListNode(l1.val if l1.val <= l2.val else l2.val)
-
-        currentResult = result
-
-        while l1 or l2:
-            if not l2 or (l1 and l1.val <= l2.val):
-                currentResult.next = ListNode(l1.val)
-                l1 = l1.next
+        currentLists = removeNones(lists)
+        resStart = None
+        while len(currentLists) > 0:
+            currentMinor = 0
+            for i, c in enumerate(currentLists):
+                currentMinor = i if currentLists[i].val < currentLists[currentMinor].val else currentMinor
+            if res:
+                res.next = ListNode(currentLists[currentMinor].val)
+                res = res.next
             else:
-                currentResult.next = ListNode(l2.val)
-                l2 = l2.next
-            currentResult = currentResult.next
+                resStart = ListNode(currentLists[currentMinor].val)
+                res = resStart
 
-        return result.next
+            if(currentLists[currentMinor].next):
+                currentLists[currentMinor] = currentLists[currentMinor].next
+            else:
+                currentLists = currentLists[0:currentMinor] + currentLists[currentMinor + 1:]
+
+        return resStart
+
+
 
 
 if __name__ == '__main__':
     n1 = ListNode(2)
     n2 = ListNode(-1)
-    # n3 = ListNode(3)
+    n3 = ListNode(3)
 
     #n1.next = n2
-    #n2.next = n3
+    n2.next = n3
 
     l1 = [n1, None, n2]
 
     s1 = Solution().mergeKLists(l1)
 
-    n = s1
-    while n:
-        print(n.val)
-        n = n.next
+    while (s1):
+        print(s1.val)
+        s1 = s1.next
