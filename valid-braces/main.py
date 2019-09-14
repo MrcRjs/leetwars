46b47e0558b26591db982106cfc01d2ad8042d1a
Marco A. Rojas <marco.rojas@alumno.buap.mx>
2019-09-14 03:26:41 -0400

    Codewars: Valid braces https://www.codewars.com/kata/valid-braces/python

---
+++ valid-braces/main.py
@@ -0,0 +1,6 @@
+def validBraces(string):
+    while "()" in string or "{}" in string or "[]" in string:
+        string = string.replace("()","")
+        string = string.replace("{}","")
+        string = string.replace("[]","")
+    return string is ""
