147b01e5e832907fd40beeb4a473f140bb15a2a1
Marco Rojas <devmarcorojas@gmail.com>
2020-11-30 23:57:10 -0500

    Generate project script

--- generate-proj/newproj.sh
+++ generate-proj/newproj.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 echo "Creating new JS project: $1"
 mkdir $1
-SCRIPTDIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SCRIPTDIR="$(readlink -f "$0")"
 DEST="$(pwd)/$1/"
 cp $SCRIPTDIR/jsproj/script.test.js $DEST/$1.test.js
 cp $SCRIPTDIR/jsproj/script.js $DEST/$1.js
 cp $SCRIPTDIR/jsproj/package.json $DEST/ 
 
 
-sed -i s/script/$1/g $DEST/$1.test.js
-sed -i s/script/$1/g $DEST/$1.js
-sed -i s/projectname/$1/g $DEST/package.json
+sed -i s/script/$1/g $DEST$1.test.js
+sed -i s/script/$1/g $DEST$1.js
+sed -i s/projectname/$1/g $DESTpackage.json
 
 cd $DEST
 
