96c968136fbc1782a1169531d50d44ed37aa3fbc
Marco Rojas <devmarcorojas@gmail.com>
2020-12-06 04:10:01 -0500

    [Codewars][5 kyu] Cat classes

--- closures/closures.js
+++ closures/closures.js
@@ -6,28 +6,28 @@ var Cat = (
             totalWeight: 0
         }
         function constructor(name, weight) {
-            
             if (!name || !weight) {
                 throw new Error('Missing name or weight')
             }
-            averageTracker.catCount++;
 
             Object.defineProperty(this, 'weight', {
                 get() { return this._weight || 0; },
                 set(newW) {
                     averageTracker.totalWeight = averageTracker.totalWeight - this.weight + newW,
-                    averageTracker.avgWeight = averageTracker.totalWeight / averageTracker.catCount
-                    return this._weight = newW; 
+                        averageTracker.avgWeight = averageTracker.totalWeight / averageTracker.catCount
+                    this._weight = newW;
                 }
             });
-            this.name =  name;
-            this.weight = weight;            
+
+            averageTracker.catCount++;
+            this.name = name;
+            this.weight = weight;
         }
-        
+
         constructor.averageWeight = function () {
             return averageTracker.avgWeight;
         }
-        
+
         return constructor;
     }()
 );
