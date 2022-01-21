671befccf00251a5d82ec2c12525ab7bbf6316f7
Restyled.io <commits@restyled.io>
2022-01-21 14:47:19 +0000

    Restyled by clang-format

--- lt-search-insert.cpp
+++ lt-search-insert.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
-        int right = nums.size() - 1;
-        int left = 0;
-        int pivot;
-        while(left <= right) {
-            pivot = ((right - left) >> 1) + left;
+  int searchInsert(vector<int> &nums, int target) {
+    int right = nums.size() - 1;
+    int left = 0;
+    int pivot;
+    while (left <= right) {
+      pivot = ((right - left) >> 1) + left;
 
-            if(nums[pivot] == target)
-            {
-                return pivot;
-            }
-            else if(nums[pivot] > target) {
-                right = pivot -1;
-            } else {
-                left = pivot + 1;
-            }
-        }
-        return left;
+      if (nums[pivot] == target) {
+        return pivot;
+      } else if (nums[pivot] > target) {
+        right = pivot - 1;
+      } else {
+        left = pivot + 1;
+      }
     }
+    return left;
+  }
 };
