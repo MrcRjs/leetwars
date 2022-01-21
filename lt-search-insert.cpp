9da50cc4243cdbb37680983335e4793eaa5ea31d
Marco Rojas <devMarcoRojas@gmail.com>
2022-01-21 09:46:36 -0500

    Create SearchInsert.cpp
    https://leetcode.com/problems/search-insert-position/

---
+++ lt-search-insert.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int right = nums.size() - 1;
+        int left = 0;
+        int pivot;
+        while(left <= right) {
+            pivot = ((right - left) >> 1) + left;
+            
+            if(nums[pivot] == target)
+            {
+                return pivot;
+            }
+            else if(nums[pivot] > target) {
+                right = pivot -1;
+            } else {
+                left = pivot + 1;
+            }    
+        }
+        return left;
+    }
+};
