10cb698d55f03b64ab23c9b6230c7873eb73b4e4
Restyled.io <commits@restyled.io>
2022-01-21 13:54:27 +0000

    Restyled by clang-format

--- firstBadVersionV2.cpp
+++ firstBadVersionV2.cpp
@@ -3,23 +3,22 @@
 
 class Solution {
 public:
-    int firstBadVersion(int n) {
-        int right = n;
-        int left = 1;
-        int pivot;
-        while(left < right) {
-            pivot = ((right - left) >> 1) + left;
-            // Its on the left side, we reduce right
-            if(isBadVersion(pivot)) {
-                right = pivot;
-            }
-            // Its on the right side we reduce left
-            else {
-                left = pivot + 1;
-            }
-
-        }
-
-        return left;
+  int firstBadVersion(int n) {
+    int right = n;
+    int left = 1;
+    int pivot;
+    while (left < right) {
+      pivot = ((right - left) >> 1) + left;
+      // Its on the left side, we reduce right
+      if (isBadVersion(pivot)) {
+        right = pivot;
+      }
+      // Its on the right side we reduce left
+      else {
+        left = pivot + 1;
+      }
     }
+
+    return left;
+  }
 };
