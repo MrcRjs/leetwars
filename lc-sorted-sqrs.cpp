d4c5d05bd9279e3d482d6da5c55545cac88bc1ac
Marco Rojas <devMarcoRojas@gmail.com>
2022-01-22 22:40:32 -0500

    Create lc-sorted-sqrs.cpp

---
+++ lc-sorted-sqrs.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        // split by middle(where there's a negative next to positive)
+        // Square both arrays
+        // reverse left side(negatives)
+        // merge smaller into large one
+    }
+};
