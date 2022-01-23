df7f5f6b64c22c9991a721a1477fcbf7c557118b
Marco Rojas <devMarcoRojas@gmail.com>
2022-01-23 14:36:01 -0500

    Create lc-sorted-sqrs.cpp

--- lc-sorted-sqrs.cpp
+++ lc-sorted-sqrs.cpp
@@ -1,9 +1,24 @@
+
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        // split by middle(where there's a negative next to positive)
-        // Square both arrays
-        // reverse left side(negatives)
-        // merge smaller into large one
+                
+        int total = nums.size();
+        vector<int> res(total);
+        int left = 0;
+        int right = total - 1;
+
+        for (int i = total - 1; i >= 0; i--) {
+            int square;
+            if (abs(nums[left]) < abs(nums[right])) {
+                square = nums[right];
+                right--;
+            } else {
+                square = nums[left];
+                left++;
+            }
+            res[i] = pow(square, 2);
+        }
+        return res;
     }
 };
