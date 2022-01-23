4a7c8cebd2ca989e23aca576889367e3ead2af60
Restyled.io <commits@restyled.io>
2022-01-23 19:36:52 +0000

    Restyled by clang-format

--- lc-sorted-sqrs.cpp
+++ lc-sorted-sqrs.cpp
@@ -1,24 +1,24 @@
 
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& nums) {
+  vector<int> sortedSquares(vector<int> &nums) {
 
-        int total = nums.size();
-        vector<int> res(total);
-        int left = 0;
-        int right = total - 1;
+    int total = nums.size();
+    vector<int> res(total);
+    int left = 0;
+    int right = total - 1;
 
-        for (int i = total - 1; i >= 0; i--) {
-            int square;
-            if (abs(nums[left]) < abs(nums[right])) {
-                square = nums[right];
-                right--;
-            } else {
-                square = nums[left];
-                left++;
-            }
-            res[i] = pow(square, 2);
-        }
-        return res;
+    for (int i = total - 1; i >= 0; i--) {
+      int square;
+      if (abs(nums[left]) < abs(nums[right])) {
+        square = nums[right];
+        right--;
+      } else {
+        square = nums[left];
+        left++;
+      }
+      res[i] = pow(square, 2);
     }
+    return res;
+  }
 };
