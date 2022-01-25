200407d2f5285278c1117c2779e31113cd9f00cd
Restyled.io <commits@restyled.io>
2022-01-25 04:21:00 +0000

    Restyled by clang-format

--- lt-maxsubarray.cpp
+++ lt-maxsubarray.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int maxSubArrayLen(vector<int>& nums, int k) {
-        unsigned int n = nums.size();
-        int prefixSum = 0;
-        int longestSubarr = 0;
-        unordered_map<int, int> indices;
+  int maxSubArrayLen(vector<int> &nums, int k) {
+    unsigned int n = nums.size();
+    int prefixSum = 0;
+    int longestSubarr = 0;
+    unordered_map<int, int> indices;
 
-        for (int i = 0; i < n; i++) {
-            prefixSum += nums[i];
+    for (int i = 0; i < n; i++) {
+      prefixSum += nums[i];
 
-            if (prefixSum == k) {
-                longestSubarr = i + 1;
-            }
+      if (prefixSum == k) {
+        longestSubarr = i + 1;
+      }
 
-            // if the sum  is not defined
-            if (indices.find(prefixSum - k) != indices.end()) {
-                longestSubarr = max(longestSubarr, i - indices[prefixSum - k]);
-            }
+      // if the sum  is not defined
+      if (indices.find(prefixSum - k) != indices.end()) {
+        longestSubarr = max(longestSubarr, i - indices[prefixSum - k]);
+      }
 
-            if (indices.find(prefixSum) == indices.end()) {
-                indices[prefixSum] = i;
-            }
-        }
-
-        return longestSubarr;
+      if (indices.find(prefixSum) == indices.end()) {
+        indices[prefixSum] = i;
+      }
     }
+
+    return longestSubarr;
+  }
 };
