c5d4b5d01c57c85f795adc183f7bd256358edc82
Restyled.io <commits@restyled.io>
2022-01-26 13:56:22 +0000

    Restyled by clang-format

--- lc-oddlength-subsum.cpp
+++ lc-oddlength-subsum.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int sumOddLengthSubarrays(vector<int>& arr) {
-        int sum = 0;
-        int arrSize = arr.size();
-        int subArrLength = 1;
-        while(subArrLength <= arrSize) {
-            int start = 0;
-            while(start + subArrLength <= arrSize) {
-                for(int i = start; i < subArrLength + start; i++) {
-                    sum += arr[i];
-                }
-                start++;
-            }
-            subArrLength += 2;
+  int sumOddLengthSubarrays(vector<int> &arr) {
+    int sum = 0;
+    int arrSize = arr.size();
+    int subArrLength = 1;
+    while (subArrLength <= arrSize) {
+      int start = 0;
+      while (start + subArrLength <= arrSize) {
+        for (int i = start; i < subArrLength + start; i++) {
+          sum += arr[i];
         }
-        return sum;
+        start++;
+      }
+      subArrLength += 2;
     }
+    return sum;
+  }
 };
