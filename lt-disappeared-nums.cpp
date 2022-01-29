8430a7f6a83499aa67976617b52e426d1190c5fc
Restyled.io <commits@restyled.io>
2022-01-29 03:26:10 +0000

    Restyled by clang-format

--- lt-disappeared-nums.cpp
+++ lt-disappeared-nums.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    vector<int> findDisappearedNumbers(vector<int>& nums) {
-        int n = nums.size() + 1;
-        std::vector <bool> existingNums(n, false);
-        int total = n;
-        for(int i = 0; i < n - 1; i++) {
-            existingNums[nums[i]] = true;
-            total--;
-        }
-
-        std::vector <int> result;
-        for(int i = 1; i < n; i++) {
-            if(!existingNums[i]) {
-                result.push_back(i);
-            }
-        }
-
-        return result;
+  vector<int> findDisappearedNumbers(vector<int> &nums) {
+    int n = nums.size() + 1;
+    std::vector<bool> existingNums(n, false);
+    int total = n;
+    for (int i = 0; i < n - 1; i++) {
+      existingNums[nums[i]] = true;
+      total--;
     }
+
+    std::vector<int> result;
+    for (int i = 1; i < n; i++) {
+      if (!existingNums[i]) {
+        result.push_back(i);
+      }
+    }
+
+    return result;
+  }
 };
