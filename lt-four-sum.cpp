d99e39f2b3363f0805e630cd80c0f6b9177f6789
Marco Rojas <devMarcoRojas@gmail.com>
2022-01-30 00:33:38 -0500

    Create lt-four-sum.cpp
    https://leetcode.com/problems/4sum-ii/

---
+++ lt-four-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        int count = 0;
+        unordered_map<int, int> map;
+        for (int n1 : nums1){
+            for (int n2 : nums2) {
+                ++map[n1 + n2];
+            }
+        }
+        
+        for (int n3 : nums3) {
+            for (int n4 : nums4) {
+                auto it = map.find(-(n3 + n4));
+                if (it != map.end()) {
+                    count += it->second;
+                }
+            }
+        }
+        return count;
+    }
+};
