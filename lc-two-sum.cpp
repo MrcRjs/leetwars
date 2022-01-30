69ba3a0b7311da760896b2587a5f1bf8d7fe3b92
Restyled.io <commits@restyled.io>
2022-01-30 04:49:46 +0000

    Restyled by clang-format

--- lc-two-sum.cpp
+++ lc-two-sum.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int low = 0;
-        int high = numbers.size() - 1;
-        while (low < high) {
-            int sum = numbers[low] + numbers[high];
+  vector<int> twoSum(vector<int> &numbers, int target) {
+    int low = 0;
+    int high = numbers.size() - 1;
+    while (low < high) {
+      int sum = numbers[low] + numbers[high];
 
-            if (sum == target) {
-                return {low + 1, high + 1};
-            } else if (sum < target) {
-                ++low;
-            } else {
-                --high;
-            }
-        }
-        return {-1, -1};
+      if (sum == target) {
+        return {low + 1, high + 1};
+      } else if (sum < target) {
+        ++low;
+      } else {
+        --high;
+      }
     }
+    return {-1, -1};
+  }
 };
