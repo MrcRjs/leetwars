e89e4234fb4e04bd5e97f99ee201c09c58b9f511
Restyled.io <commits@restyled.io>
2022-01-31 03:35:44 +0000

    Restyled by clang-format

--- lt-reverse-string.cpp
+++ lt-reverse-string.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    void reverseString(vector<char>& s) {
-        int n = s.size() - 1;
-        for(int i = 0; i <= n / 2; i++) {
-            char t = s[n - i];
-            s[n - i] = s[i];
-            s[i] = t;
-        }
+  void reverseString(vector<char> &s) {
+    int n = s.size() - 1;
+    for (int i = 0; i <= n / 2; i++) {
+      char t = s[n - i];
+      s[n - i] = s[i];
+      s[i] = t;
     }
+  }
 };
