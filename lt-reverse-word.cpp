8766d35d7fd6cd9472a1f9dd2ac266f756a5f874
Restyled.io <commits@restyled.io>
2022-01-31 04:28:07 +0000

    Restyled by clang-format

--- lt-reverse-word.cpp
+++ lt-reverse-word.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-        int n = s.size();
-        int wordStart = 0;
-        for(int i = 0; i < n; i++) {
-            // if current char is a space
-            if(s.at(i) == ' ') {
-                string reversed = reverseString(s.substr(wordStart, i - wordStart));
-                s.replace(wordStart, i - wordStart, reversed);
-                wordStart = i + 1;
-            } else if(i == n - 1) {
-                string reversed = reverseString(s.substr(wordStart, i - wordStart + 1));
-                s.replace(wordStart, i - wordStart + 1, reversed);
-            };
-
-        }
-        return s;
+  string reverseWords(string s) {
+    int n = s.size();
+    int wordStart = 0;
+    for (int i = 0; i < n; i++) {
+      // if current char is a space
+      if (s.at(i) == ' ') {
+        string reversed = reverseString(s.substr(wordStart, i - wordStart));
+        s.replace(wordStart, i - wordStart, reversed);
+        wordStart = i + 1;
+      } else if (i == n - 1) {
+        string reversed = reverseString(s.substr(wordStart, i - wordStart + 1));
+        s.replace(wordStart, i - wordStart + 1, reversed);
+      };
     }
+    return s;
+  }
 
-    string reverseString(string s) {
-        int n = s.size() - 1;
-        for(int i = 0; i <= n / 2; i++) {
-            char t = s[n - i];
-            s[n - i] = s[i];
-            s[i] = t;
-        }
-        return s;
+  string reverseString(string s) {
+    int n = s.size() - 1;
+    for (int i = 0; i <= n / 2; i++) {
+      char t = s[n - i];
+      s[n - i] = s[i];
+      s[i] = t;
     }
+    return s;
+  }
 };
