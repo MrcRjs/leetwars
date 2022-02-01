fc5d969d63c8f5435d8cba07b5dd5281e0f8b911
Restyled.io <commits@restyled.io>
2022-02-01 04:19:08 +0000

    Restyled by astyle

--- lt-roadtrip.cpp
+++ lt-roadtrip.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-    int n = gas.size();
+        int n = gas.size();
 
-    int total = 0;
-    int current = 0;
-    int start = 0;
-    for (int i = 0; i < n; i++) {
-      total += gas[i] - cost[i];
-      current += gas[i] - cost[i];
-      // Not enough gas
-      if (current < 0) {
-        // Set next gas station
-        start = i + 1;
-        current = 0;
-      }
-    }
-    return total >= 0 ? start : -1;
+        int total = 0;
+        int current = 0;
+        int start = 0;
+        for (int i = 0; i < n; i++) {
+            total += gas[i] - cost[i];
+            current += gas[i] - cost[i];
+            // Not enough gas
+            if (current < 0) {
+                // Set next gas station
+                start = i + 1;
+                current = 0;
+            }
+        }
+        return total >= 0 ? start : -1;
     }
 };
 
