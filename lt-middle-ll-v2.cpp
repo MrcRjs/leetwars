5363aa9dc5c974d05de27d10c7718ce0b7e01f34
Marco Rojas <devMarcoRojas@gmail.com>
2022-02-01 11:57:15 -0500

    Create lt-middle-ll-v2.cpp
    https://leetcode.com/problems/middle-of-the-linked-list/

---
+++ lt-middle-ll-v2.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode * middle = head;
+        ListNode * current = head;
+        while(current != NULL && current->next != NULL){
+            middle = middle->next;
+            current = current->next->next;
+        }
+        
+        return middle;
+    }
+};
