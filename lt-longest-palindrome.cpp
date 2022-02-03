617dd10300c25191a7b9a27636eb60cc26186d59
Restyled.io <commits@restyled.io>
2022-02-03 03:51:43 +0000

    Restyled by clang-format

--- lt-longest-palindrome.cpp
+++ lt-longest-palindrome.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int longestPalindrome(string s) {
-        int count = 0;
-        for(int i = 0; i < s.size(); i++) {
-            if(s[i]!='-') {
-                size_t found = s.find(s[i], i + 1);
-                if(found!=std::string::npos) {
-                    s.replace(i, 1, "-");
-                    s.replace(found, 1, "-");
-                    count+=2;
-
-                }
-            }
+  int longestPalindrome(string s) {
+    int count = 0;
+    for (int i = 0; i < s.size(); i++) {
+      if (s[i] != '-') {
+        size_t found = s.find(s[i], i + 1);
+        if (found != std::string::npos) {
+          s.replace(i, 1, "-");
+          s.replace(found, 1, "-");
+          count += 2;
         }
-        return (count) < s.size() ? count + 1 : count;
+      }
     }
+    return (count) < s.size() ? count + 1 : count;
+  }
 };
