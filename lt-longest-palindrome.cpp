b21b556995117621d9dc3ffa2fd8684da1cba2de
Restyled.io <commits@restyled.io>
2022-02-03 03:51:31 +0000

    Restyled by astyle

--- lt-longest-palindrome.cpp
+++ lt-longest-palindrome.cpp
@@ -3,13 +3,13 @@ public:
     int longestPalindrome(string s) {
         int count = 0;
         for(int i = 0; i < s.size(); i++) {
-            if(s[i]!='-'){
+            if(s[i]!='-') {
                 size_t found = s.find(s[i], i + 1);
-                if(found!=std::string::npos){
+                if(found!=std::string::npos) {
                     s.replace(i, 1, "-");
                     s.replace(found, 1, "-");
                     count+=2;
-                
+
                 }
             }
         }
