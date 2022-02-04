e6e9bc068dcf57ff1d443fbf2a5fa7d1772a39fd
Restyled.io <commits@restyled.io>
2022-02-04 18:45:38 +0000

    Restyled by astyle

--- lt-stringshift.cpp
+++ lt-stringshift.cpp
@@ -3,15 +3,15 @@ public:
     string stringShift(string s, vector<vector<int>>& shift) {
         int clockwise = 0;
         for (auto move : shift) {
-            if(move[0] == 1){
+            if(move[0] == 1) {
                 move[1] = -move[1];
             }
             clockwise += move[1];
         }
-        if(clockwise == 0){
+        if(clockwise == 0) {
             return s;
         }
-        else if(clockwise > 0){
+        else if(clockwise > 0) {
             clockwise = clockwise % s.length();
             return s.substr(clockwise) + s.substr(0, clockwise);
         }
