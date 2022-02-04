e69774cf3ad18b4df2c85c1deef54a3efa2c9cbd
Restyled.io <commits@restyled.io>
2022-02-04 18:45:40 +0000

    Restyled by clang-format

--- lt-stringshift.cpp
+++ lt-stringshift.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    string stringShift(string s, vector<vector<int>>& shift) {
-        int clockwise = 0;
-        for (auto move : shift) {
-            if(move[0] == 1) {
-                move[1] = -move[1];
-            }
-            clockwise += move[1];
-        }
-        if(clockwise == 0) {
-            return s;
-        }
-        else if(clockwise > 0) {
-            clockwise = clockwise % s.length();
-            return s.substr(clockwise) + s.substr(0, clockwise);
-        }
-        else {
-            clockwise = abs(clockwise) % s.length();
-            return  s.substr(s.length() - clockwise) + s.substr(0, s.length() - clockwise);
-        }
+  string stringShift(string s, vector<vector<int>> &shift) {
+    int clockwise = 0;
+    for (auto move : shift) {
+      if (move[0] == 1) {
+        move[1] = -move[1];
+      }
+      clockwise += move[1];
     }
+    if (clockwise == 0) {
+      return s;
+    } else if (clockwise > 0) {
+      clockwise = clockwise % s.length();
+      return s.substr(clockwise) + s.substr(0, clockwise);
+    } else {
+      clockwise = abs(clockwise) % s.length();
+      return s.substr(s.length() - clockwise) +
+             s.substr(0, s.length() - clockwise);
+    }
+  }
 };
