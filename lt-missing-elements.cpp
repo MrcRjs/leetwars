6daf6ee1cbafbbdfc1f05d8d497cccdc8a0a6bb4
Restyled.io <commits@restyled.io>
2022-02-06 04:40:35 +0000

    Restyled by clang-format

--- lt-missing-elements.cpp
+++ lt-missing-elements.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
+  int missing(int idx, vector<int> nums) { return nums[idx] - nums[0] - idx; }
+  int missingElement(vector<int> &nums, int k) {
+    int n = nums.size();
 
-    int missing(int idx, vector<int> nums) {
-        return nums[idx] - nums[0] - idx;
+    if (k > missing(n - 1, nums)) {
+      return nums[n - 1] + k - missing(n - 1, nums);
     }
-    int missingElement(vector<int>& nums, int k) {
-        int n = nums.size();
 
-        if(k > missing(n - 1, nums)) {
-            return nums[n - 1] + k - missing(n - 1, nums);
-        }
+    int idx = 1;
 
-        int idx = 1;
-
-        while(missing(idx, nums) < k) {
-            idx++;
-        }
-
-        return nums[idx - 1] + k - missing(idx - 1, nums);
+    while (missing(idx, nums) < k) {
+      idx++;
     }
+
+    return nums[idx - 1] + k - missing(idx - 1, nums);
+  }
 };
