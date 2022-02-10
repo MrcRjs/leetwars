9dd97d9ebdbd0f50e9fd931fbbc1ba6d3bb58c73
Restyled.io <commits@restyled.io>
2022-02-10 03:34:45 +0000

    Restyled by clang-format

--- lt-findships.cpp
+++ lt-findships.cpp
@@ -9,26 +9,26 @@
 
 class Solution {
 public:
-    int countShips(Sea sea, vector<int> topRight, vector<int> bottomLeft) {
+  int countShips(Sea sea, vector<int> topRight, vector<int> bottomLeft) {
 
-        if (bottomLeft[0] > topRight[0] || bottomLeft[1] > topRight[1]) {
-            return 0;
-        }
-
-        if (!sea.hasShips(topRight, bottomLeft)) {
-            return 0;
-        }
-
-        // One square ship
-        if (topRight[0] == bottomLeft[0] && topRight[1] == bottomLeft[1]) {
-            return 1;
-        }
-
-        int midX = (topRight[0] + bottomLeft[0]) / 2;
-        int midY = (topRight[1] + bottomLeft[1]) / 2;
-        return countShips(sea, {midX, midY}, bottomLeft) +
-               countShips(sea, topRight, {midX + 1, midY + 1}) +
-               countShips(sea, {midX, topRight[1]}, {bottomLeft[0], midY + 1}) +
-               countShips(sea, {topRight[0], midY}, {midX + 1, bottomLeft[1]});
+    if (bottomLeft[0] > topRight[0] || bottomLeft[1] > topRight[1]) {
+      return 0;
     }
+
+    if (!sea.hasShips(topRight, bottomLeft)) {
+      return 0;
+    }
+
+    // One square ship
+    if (topRight[0] == bottomLeft[0] && topRight[1] == bottomLeft[1]) {
+      return 1;
+    }
+
+    int midX = (topRight[0] + bottomLeft[0]) / 2;
+    int midY = (topRight[1] + bottomLeft[1]) / 2;
+    return countShips(sea, {midX, midY}, bottomLeft) +
+           countShips(sea, topRight, {midX + 1, midY + 1}) +
+           countShips(sea, {midX, topRight[1]}, {bottomLeft[0], midY + 1}) +
+           countShips(sea, {topRight[0], midY}, {midX + 1, bottomLeft[1]});
+  }
 };
