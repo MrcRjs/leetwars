c0d29cccae77248b80c9601c3a5a9869e4658a6b
Restyled.io <commits@restyled.io>
2022-02-10 04:32:53 +0000

    Restyled by clang-format

--- lc-dna-sequence.cpp
+++ lc-dna-sequence.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    vector<string> findRepeatedDnaSequences(string s) {
-        int DNA_SIZE = 10;
-        if(s.length() < DNA_SIZE) {
-            return {};
-        }
-
-        vector <string> result;
-        map<string, int> dna;
-        for(int i = 0; i < s.length() - DNA_SIZE + 1; i++) {
-            string current = s.substr(i, DNA_SIZE);
-            if(dna[current]) {
-                if(dna[current] == 1) {
-                    result.push_back(current);
-                }
-                dna[current]++;
-            } else {
-                dna[current] = 1;
-            }
-        }
-
-
-
-        return result;
+  vector<string> findRepeatedDnaSequences(string s) {
+    int DNA_SIZE = 10;
+    if (s.length() < DNA_SIZE) {
+      return {};
     }
+
+    vector<string> result;
+    map<string, int> dna;
+    for (int i = 0; i < s.length() - DNA_SIZE + 1; i++) {
+      string current = s.substr(i, DNA_SIZE);
+      if (dna[current]) {
+        if (dna[current] == 1) {
+          result.push_back(current);
+        }
+        dna[current]++;
+      } else {
+        dna[current] = 1;
+      }
+    }
+
+    return result;
+  }
 };
