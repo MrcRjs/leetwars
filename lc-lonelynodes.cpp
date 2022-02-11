75f6cf867ed10fe4bea31803b9e65526b58d365b
Restyled.io <commits@restyled.io>
2022-02-11 03:09:59 +0000

    Restyled by clang-format

--- lc-lonelynodes.cpp
+++ lc-lonelynodes.cpp
@@ -6,46 +6,46 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 #include <queue>
 class Solution {
 public:
-    bool hasSingleChild(TreeNode * root);
+  bool hasSingleChild(TreeNode *root);
 
-    TreeNode * getSingleChild(TreeNode * root);
+  TreeNode *getSingleChild(TreeNode *root);
 
-    vector<int> getLonelyNodes(TreeNode * root) {
-        vector<int> lonelynodes;
-        queue<TreeNode *> nodeQ;
-        nodeQ.push(root);
-        while (nodeQ.size()) {
-            TreeNode * current = nodeQ.front();
-            nodeQ.pop();
+  vector<int> getLonelyNodes(TreeNode *root) {
+    vector<int> lonelynodes;
+    queue<TreeNode *> nodeQ;
+    nodeQ.push(root);
+    while (nodeQ.size()) {
+      TreeNode *current = nodeQ.front();
+      nodeQ.pop();
 
-            if (hasSingleChild(current)) {
-                lonelynodes.push_back(getSingleChild(current)->val);
-            }
+      if (hasSingleChild(current)) {
+        lonelynodes.push_back(getSingleChild(current)->val);
+      }
 
-            if (current->left) {
-                nodeQ.push(current->left);
-            }
+      if (current->left) {
+        nodeQ.push(current->left);
+      }
 
-            if (current->right) {
-                nodeQ.push(current->right);
-            }
-        };
+      if (current->right) {
+        nodeQ.push(current->right);
+      }
+    };
 
-        return lonelynodes;
-    }
-
+    return lonelynodes;
+  }
 };
 
 bool Solution::hasSingleChild(TreeNode *root) {
-    return (!root->left && root->right) || (root->left && !root->right);
+  return (!root->left && root->right) || (root->left && !root->right);
 };
 
 TreeNode *Solution::getSingleChild(TreeNode *root) {
-    return root->left ? root->left : root->right;
+  return root->left ? root->left : root->right;
 };
